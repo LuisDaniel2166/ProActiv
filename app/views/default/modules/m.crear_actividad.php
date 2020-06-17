c36789e30eec586dde73a7636eca69f7316205e4
Omar0906 <Omar0906@users.noreply.github.com>
2020-06-17 11:39:36 -0500

    Create m.crear_actividad.php

---
+++ app/views/default/modules/m.crear_actividad.php
@@ -0,0 +1,62 @@
+<div id="wrapper">
+
+<div class="section bgcolor noover">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="tagline-message">
+                    <h3>Crear Actividad</h3>
+                </div>
+            </div><!-- end col -->
+        </div><!-- end row -->
+    </div><!-- end container -->
+</div><!-- end section -->
+
+<section class="section">
+    <div class="container">
+                    <h3>Nombre de la actividad:</h3>
+                    <input type="text" name="nombre_act" class="form-control" placeholder="Nombre de la actividad">
+                    <h3>Descripcion de la actividad:</h3>
+                    <input type="text" class="form-control" placeholder="Descripcion de la actividad">
+                    <h3>Seleccionar integrantes:</h3>
+                    <select>
+                      <option value="1">Luis Daniel Mendez Castellanos</option>
+                      <option value="2">Keiry Yoseli Rodriguez Gonzalez</option>
+                      <option value="3">Alexis Torres Acosta</option>
+                      <option value="4">Fernando Miramontes Alvarez</option>
+                      <option value="5">Omar Oswaldo Rivera Flores</option>
+                    </select>
+                    <h4>Integrantes a registrar en la actividad</h4>
+                    <ul>
+                        <li>Alexis Torres Acosta</li>
+                        <li>Keiry Yoseli Rogriguez Gonzalez</li>
+                    </ul>
+                    <h4>Fecha de inicio:</h4>
+                    <p><input type="date" id="start" name="trip-start" value="2019-12-02" min="2019-12-02" max="2020-12-31"></p>
+                    <h4>Fecha de finalización:</h4>
+                    <p><input type="date" id="start" name="trip-start" value="2019-12-02" min="2019-12-02" max="2020-12-31"></p>
+                        <p><button type="submit" class="btn btn-default" role="link" onclick="window.location='mis_actividades.html'">Crear actividad</button></p>
+                        
+    </div><!-- end container -->
+</section>
+
+<div class="section copyrights">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3">
+                <div class="cop-logo">
+                    <img src="images/logobig.png" alt="" width="80%" height="80%">
+                </div>
+            </div>
+            <div class="col-lg-9 col-md-9 text-right">
+                <div class="cop-links">
+                    <ul class="list-inline">
+                        <li>&copy; 2019 S.O.P.A (Sistema Organizador de Proyectos y Actividades)</li>
+                        <li>Versión: Pre-Alpha</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div><!-- end wrapper -->
